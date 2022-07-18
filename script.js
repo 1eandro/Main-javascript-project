9a07f69d9b3d3367e9173ede172b98122d3ada97
David Fortunato <david.fortunato05@gmail.com>
2022-07-18 14:59:25 +0300

    rock, paper,scissors

---
+++ script.js
@@ -0,0 +1,15 @@
+var introduction = prompt(" make your choice!, rock, paper, scissors");
+const playerSelection = ["rock ,paper, scissors"]
+const computerSelection = ["ROCK, PAPER, SCISSORS"];
+let playerScore = 0;
+let computerScore = 0;
+let result;
+
+function computerPlay (){
+  const choiceNumber = Math.floor(Math.random()*3);
+  const computerChoice = computerSelection[choiceNumber];
+
+}
+
+
+console.log(choiceNumber);
