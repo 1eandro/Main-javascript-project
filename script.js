fb9f29cad2d71d693ee74a14dc2156e731b7893a
David Fortunato <david.fortunato05@gmail.com>
2022-07-18 17:34:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,58 @@
-var introduction = prompt(" make your choice!, rock, paper, scissors");
-const playerSelection = ["rock ,paper, scissors"]
-const computerSelection = ["ROCK, PAPER, SCISSORS"];
-let playerScore = 0;
-let computerScore = 0;
-let result;
-
-function computerPlay (){
-  const choiceNumber = Math.floor(Math.random()*3);
-  const computerChoice = computerSelection[choiceNumber];
-
-}
 
 
-console.log(choiceNumber);
+  const introduction = prompt(" make your choice!, rock, paper, scissors");
+  const playerSelection = ["rock ,paper, scissors"]
+  const computerSelection = ["ROCK, PAPER, SCISSORS"];
+  let playerScore = 0;
+  let computerScore = 0;
+  let player;
+  let computer;
+  
+
+  function computerPlay (){
+    Math.floor(Math.random()*3);
+    
+  }
+
+  
+
+  function winner () {
+    if (player === computer){
+      resut.textContent = "draw"
+    }
+
+    else if (player == "rock"){
+      if( computer == "paper"){
+        result.textContent = "computer won";
+
+        computerScore++;
+
+      }else{
+        result.textContent = "player won"
+        playerScore++;
+      }
+      
+    }
+    
+    else if (player == "scissors"){
+      if (computer == "rock"){
+        result.textContent = "computer won"
+        computerScore++;
+      }else{
+        result.textContent ="player won";
+        playerScore++;
+      }
+    }
+    else if (player == "paper"){
+      if(computer == "scissors"){
+        result.textContent = "computer won";
+        computerScore++;
+      }
+      else{
+        result.textContent = "player won";
+        playerScore++;
+      }
+    }
+  }
+
+  
