24028b4f5b384d84eb80ef3acfa83bf83d978b26
David Fortunato <david.fortunato05@gmail.com>
2022-07-21 15:04:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,78 +1,60 @@
+let introduction = prompt(' make your choice!, rock, paper, scissors');
+let computerChoice = ['rock', 'paper', 'scissors'];
 
+let introductionScore = 0;
+let computerScore = 0;
 
-  let introduction = prompt(" make your choice!, rock, paper, scissors");
-  let computerChoice = ["rock , paper, scissors"]
-  
- 
-  let introductionScore = 0;
-  let computerScore = 0;
+let rock = 1;
+let paper = 2;
+let scissors = 3;
 
-  let rock = 1;
-  let paper = 2;
-  let scissors = 3;
+function computerPlay() {
+  let computerChoice = Math.floor(Math.random() * 3);
 
+  if (computerChoice === 1) {
+    return 'rock';
+  } else if (computerChoice === 2) {
+    return 'paper';
+  } else {
+    return 'scissors';
+  }
+}
 
-  function computerPlay (){
-    
-     let computerChoice = Math.floor(Math.random()*3);
+console.log(computerPlay(computerChoice));
+
+function winner(introduction,computerChoice) {
+  if (introduction === computerChoice) {
+    return " It's a tie";
+  } else if (introduction === 'rock') {
+    if (computerChoice === 'paper') {
+      Score++;
+      return 'computer won';
+    } else {
+      introductionScore++;
+      return ' you lose';
+    }
+  } else if (introduction === 'scissors') {
+    if (computerChoice === 'rock') {
+      computerScore++;
+      return 'computer won';
+    } else {
+      introductionScore++;
+      return 'computer won';
+    }
+  } else if (introduction === 'paper') {
+    if (computerChoice === 'scissors') {
+      computerScore++;
+      return 'computer won';
+    } else {
+      introductionScore++;
+      return 'You won';
+    }
+  }
+}
+console.log(winner(computerPlay(introduction, computerChoice)));
+
+function game() {
+  for (let i = 0; i < 5; i++) {
      
-    if (computerChoice === 1){
-      return "rock";
-    }else if ( computerChoice === 2){
-      return "paper"
-    }else{
-      return "scissors";
-    }
-
-    
   }
- 
-  console.log(computerPlay(computerChoice))
- 
-
-  function winner ( ) {
-    if (introduction === computerChoice){
-     return  " It's a tie";
-    }
-
-    else if (introduction === "rock"){
-      if( computerChoice === "paper"){
-      return "computer won";
-        Score++;
-
-      }else{
-      return " you lose";
-        introductionScore++;
-      }
-      
-    }
-    
-    else if (introduction === "scissors"){
-      if (computerChoice === "rock"){
-      return "computer won";
-        computerScore++;
-      }else{
-      return"You won";
-        introductionScore++;
-      }
-    }
-    else if (introduction === "paper"){
-      if(computerChoice === "scissors"){
-      return "computer won";
-        computerScore++;
-      }
-      else{
-      return "You won";
-        introductionScore++;
-      }
-    }
-  }
-  console.log(winner(introduction,computerChoice));
-
-
-  function game (){
-    for(let i=0;i<5;i++){
-      introductionSelection = prompt("rock, paper, Scissors");
-    }
-
-  }
+}
